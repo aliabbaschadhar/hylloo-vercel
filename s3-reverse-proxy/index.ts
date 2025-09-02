1ff6512c30e9f74a17f7d527c08c2a5a10f18b3f
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-09-02 20:51:17 +0500

    feat: reverse proxy done

--- s3-reverse-proxy/index.ts
+++ s3-reverse-proxy/index.ts
@@ -1,26 +1,28 @@
 import "dotenv/config"
 import express from "express"
-import * as httpProxy from "http-proxy"
+import httpProxy from "http-proxy"
 
 const app = express()
 const PORT = 8000
 const BASE_PATH = process.env.BASE_PATH
-const proxy = httpProxy.createProxy()
+const proxy = httpProxy.createProxyServer()
 
 
 app.use((req, res) => {
   const host = req.hostname;
-  const projectId = host.split(".")[0]
+  const projectId = host.split(".")[0];
 
-  const resolvesTo = `${BASE_PATH}/${projectId}`
+  // If the request is for root, rewrite to /index.html
+  if (req.path === "/") {
+    // req.path  // It is a readonly property
+    req.url = "/index.html";
+  }
 
-  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
-})
-
-// Right now we have to add index.html in url 
-// To avoid that we would use a configuration on our proxy.
+  const resolvesTo = `${BASE_PATH}/${projectId}`;
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
 
 
 app.listen(PORT, () => {
-  console.log(`Reverse Proxy is running... ${PORT}`)
+  console.log(`Reverse Proxy is running... ${PORT}`);
 })
